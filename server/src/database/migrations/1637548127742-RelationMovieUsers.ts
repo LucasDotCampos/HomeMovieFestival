eca142ebffa5865a4f5af6c16da31100e353dd84
juaniwk3 <juaniwk3@gmail.com>
2021-11-24 14:17:04 -0300

    inputs style

--- server/src/database/migrations/1637548127742-RelationMovieUsers.ts
+++ server/src/database/migrations/1637548127742-RelationMovieUsers.ts
@@ -1,20 +1,33 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class RelationMovieUsers1637548127742 implements MigrationInterface {
-    name = 'RelationMovieUsers1637548127742'
+  name = "RelationMovieUsers1637548127742";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "public"."movies" ADD "usersId" uuid`);
-        await queryRunner.query(`ALTER TABLE "public"."movies" DROP COLUMN "releaseDate"`);
-        await queryRunner.query(`ALTER TABLE "public"."movies" ADD "releaseDate" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "public"."movies" ADD CONSTRAINT "FK_b0edeb0b2087cd7857fa11c4790" FOREIGN KEY ("usersId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "public"."movies" ADD "usersId" uuid`);
+    await queryRunner.query(
+      `ALTER TABLE "public"."movies" DROP COLUMN "releaseDate"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "public"."movies" ADD "releaseDate" character varying NOT NULL`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "public"."movies" ADD CONSTRAINT "FK_b0edeb0b2087cd7857fa11c4790" FOREIGN KEY ("usersId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "public"."movies" DROP CONSTRAINT "FK_b0edeb0b2087cd7857fa11c4790"`);
-        await queryRunner.query(`ALTER TABLE "public"."movies" DROP COLUMN "releaseDate"`);
-        await queryRunner.query(`ALTER TABLE "public"."movies" ADD "releaseDate" date NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "public"."movies" DROP COLUMN "usersId"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "public"."movies" DROP CONSTRAINT "FK_b0edeb0b2087cd7857fa11c4790"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "public"."movies" DROP COLUMN "releaseDate"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "public"."movies" ADD "releaseDate" date NOT NULL`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "public"."movies" DROP COLUMN "usersId"`
+    );
+  }
 }
