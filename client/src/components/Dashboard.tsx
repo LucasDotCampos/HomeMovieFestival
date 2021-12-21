72bdbb7f20f2c8b32ab6333c5eb398f865cb6f83
juaniwk3 <juaniwk3@gmail.com>
2021-12-21 15:55:06 -0300

    file upload

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,43 +1,89 @@
-import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import React, { SyntheticEvent, useState } from 'react';
+import { Button, Modal, InputGroup, FormControl, Alert } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
+import { api } from '../services/api';
+import '../style/dashboard.scss';
+import FileUpload from './FileUpload';
 
 export default function Dashboard() {
   const [error, setError] = useState('');
-  const { currentUser, logout } = useAuth();
+  const { currentUser, isLogged, logout } = useAuth();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => (setShow(true), setError(''));
   const navigate = useNavigate();
+  const image = currentUser.avatar;
+  const [profPic, setProfPic] = useState(image);
+
+  const handlePicChange = async () => {
+    try {
+      await api.post('/users/updatepicture', profPic);
+    } catch (error) {
+      console.log(error);
+      setError('Failed to change profile picture');
+    }
+  };
 
   const handleLogOut = async () => {
     setError('');
 
     try {
       await logout();
-      navigate('/');
     } catch (error) {
       setError('Failed to log out');
     }
+    navigate('/');
   };
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>
-        Welcome <strong>{currentUser.name}</strong>
-      </p>
-      <div>
-        <img
-          style={{ height: '50px', marginLeft: '20px' }}
-          src={currentUser && currentUser.avatar}
-          alt=""
-        />
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text">Avatar</span>
-          </div>
-          <input className="form-control" />
-        </div>
-      </div>
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Change your profile picture</Modal.Title>
+        </Modal.Header>
+        <Modal.Body
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <div className="input-group" style={{ width: '100%' }} />
+          Preview
+          <img
+            className="pic"
+            src={profPic}
+            alt="profile-picture"
+            onClick={handleShow}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <FileUpload />
+          {error && (
+            <Alert className="h-25" variant="danger">
+              {error}
+            </Alert>
+          )}
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button type="submit" variant="primary" onClick={handlePicChange}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <img
+        className="pic"
+        src={currentUser && currentUser.avatar}
+        alt=""
+        onClick={handleShow}
+      />
+      <strong>{currentUser.name}</strong>
       <p>Email: {currentUser && currentUser.email}</p>
       <Button onClick={handleLogOut}>Log Out</Button>
       <p>{error}</p>
---
+++ client/src/services/fileValidatorService.ts
@@ -0,0 +1,35 @@
+interface ValidatorResponse {
+  isValid: boolean;
+  errorMessage: string;
+}
+
+const fileTypes = ["jpg", "jpeg", "png", "doc", "docx"];
+
+async function validateFileSize(fileSize: number): Promise<ValidatorResponse> {
+  const documentFileSizeValidator = (
+    await import("../validators/DocumentFileSizeValidator")
+  ).default;
+
+  const validator = new documentFileSizeValidator(fileSize);
+  const isValid = validator.validateFileSize();
+
+  return {
+    isValid,
+    errorMessage: isValid ? "" : validator.getErrorMessage(),
+  };
+}
+
+async function validateFileType(fileType: string): Promise<ValidatorResponse> {
+  const fileTypeValidator = (await import("../validators/FileTypeValidator"))
+    .default;
+
+  const validator = new fileTypeValidator(fileType, fileTypes);
+  const isValid = validator.validateFile();
+
+  return {
+    isValid,
+    errorMessage: isValid ? "" : validator.getErrorMessage(),
+  };
+}
+
+export { validateFileSize, validateFileType };
