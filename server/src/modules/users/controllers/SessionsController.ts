df63a6a268dd82976b480cfd1873fe54438a4b85
juaniwk3 <juaniwk3@gmail.com>
2022-01-02 02:56:36 -0300

    status code on login controller

--- server/src/modules/users/controllers/SessionsController.ts
+++ server/src/modules/users/controllers/SessionsController.ts
@@ -2,20 +2,28 @@ import { Request, Response } from "express";
 import CreateSessionsService from "../services/CreateSessionsService";
 
 export default class SessionsController {
-  public async create(request: Request, response: Response): Promise<Response> {
-    try {
-      const { email, password } = request.body;
+    public async create(
+        request: Request,
+        response: Response
+    ): Promise<Response> {
+        try {
+            const { email, password } = request.body;
 
-      const createSession = new CreateSessionsService();
+            const createSession = new CreateSessionsService();
 
-      const user = await createSession.execute({
-        email,
-        password,
-      });
+            const user = await createSession.execute({
+                email,
+                password,
+            });
 
-      return response.json(user);
-    } catch (err) {
-      return response.json(err.message);
+            response.status(400).json({
+                message: "Login Failed",
+                status: response.statusCode,
+            });
+
+            return response.json(user);
+        } catch (err) {
+            return response.json(err.message);
+        }
     }
-  }
 }
