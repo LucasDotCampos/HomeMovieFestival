8342e54876daa7be53f6e64ff7eae6aa59e4dbb2
juaniwk3 <juaniwk3@gmail.com>
2022-01-03 22:24:25 -0300

    structure

--- server/src/modules/users/controllers/UsersController.ts
+++ server/src/modules/users/controllers/UsersController.ts
@@ -5,102 +5,121 @@ import ListMoviesUserService from "../services/ListMoviesUserService";
 import DeleteUsersService from "../services/DeleteUsersService";
 import UserInformationService from "../services/UserInformationService";
 import UpdateProfileService from "../services/UpdateProfileService";
+import { getCustomRepository } from "typeorm";
+import UsersRepository from "../typeorm/repositories/UsersRepository";
 
 export default class UsersController {
-  public async searchById(
-    request: Request,
-    response: Response
-  ): Promise<Response> {
-    try {
-      const { userId } = request.params;
-      const listMoviesUser = new ListMoviesUserService();
+    public async searchById(
+        request: Request,
+        response: Response
+    ): Promise<Response> {
+        try {
+            const { userId } = request.params;
+            const listMoviesUser = new ListMoviesUserService();
 
-      const users = await listMoviesUser.execute({ userId });
+            const users = await listMoviesUser.execute({ userId });
 
-      return response.status(200).json(users);
-    } catch (err) {
-      return response.json(err.message);
+            return response.status(200).json(users);
+        } catch (err) {
+            return response.json(err.message);
+        }
     }
-  }
 
-  public async userById(
-    request: Request,
-    response: Response
-  ): Promise<Response> {
-    try {
-      const { id } = request.params;
-      const userInformationService = new UserInformationService();
+    public async userById(
+        request: Request,
+        response: Response
+    ): Promise<Response> {
+        try {
+            const { id } = request.params;
+            const userInformationService = new UserInformationService();
 
-      const user = await userInformationService.execute({ id });
+            const user = await userInformationService.execute({ id });
 
-      return response.status(200).json(user);
-    } catch (err) {
-      return response.json(err.message);
+            return response.status(200).json(user);
+        } catch (err) {
+            return response.json(err.message);
+        }
     }
-  }
 
-  public async create(request: Request, response: Response): Promise<Response> {
-    try {
-      const { name, email, password } = request.body;
+    public async create(
+        request: Request,
+        response: Response
+    ): Promise<Response> {
+        try {
+            const { name, email, password } = request.body;
 
-      const createUser = new CreateUserService();
-      const user = await createUser.execute({
-        name,
-        email,
-        password,
-        avatar: "profilepic.png",
-      });
+            const createUser = new CreateUserService();
 
-      delete user.password;
+            const usersRepository = getCustomRepository(UsersRepository);
+            const emailExists = await usersRepository.findByemail(email);
 
-      return response.status(200).json(user);
-    } catch (err) {
-      return response.json(err.message);
+            if (emailExists) {
+                response.status(400);
+            }
+
+            const user = await createUser.execute({
+                name,
+                email,
+                password,
+                avatar: "profilepic.png",
+            });
+
+            delete user.password;
+
+            return response.status(200).json(user);
+        } catch (err) {
+            return response.json(err.message);
+        }
     }
-  }
 
-  public async usersList(
-    request: Request,
-    response: Response
-  ): Promise<Response> {
-    try {
-      const ListUsersServices = new ListUsersService();
+    public async usersList(
+        request: Request,
+        response: Response
+    ): Promise<Response> {
+        try {
+            const ListUsersServices = new ListUsersService();
 
-      const users = await ListUsersServices.execute();
-      return response.json(users);
-    } catch (err) {
-      return response.json(err.message);
+            const users = await ListUsersServices.execute();
+            return response.json(users);
+        } catch (err) {
+            return response.json(err.message);
+        }
     }
-  }
 
-  public async delete(request: Request, response: Response): Promise<Response> {
-    try {
-      const { id } = request.params;
-      const deleteUsers = new DeleteUsersService();
-      await deleteUsers.execute({ id });
-      return response.status(200).json([]);
-    } catch (err) {
-      return response.status(424).json(err.message);
+    public async delete(
+        request: Request,
+        response: Response
+    ): Promise<Response> {
+        try {
+            const { id } = request.params;
+            const deleteUsers = new DeleteUsersService();
+            await deleteUsers.execute({ id });
+            return response.status(200).json([]);
+        } catch (err) {
+            return response.status(424).json(err.message);
+        }
     }
-  }
 
-  public async update(request: Request, response: Response): Promise<Response> {
-    try {
-      const userId = request.userId;
-      const { name, email, password, oldPassword } = request.body;
+    public async update(
+        request: Request,
+        response: Response
+    ): Promise<Response> {
+        try {
+            const userId = request.userId;
+            const { name, email, password, oldPassword } = request.body;
 
-      const updateProfile = new UpdateProfileService();
+            const updateProfile = new UpdateProfileService();
 
-      const user = await updateProfile.execute({
-        userId,
-        name,
-        email,
-        password,
-        oldPassword,
-      });
-      return response.json(user);
-    } catch (err) {
-      return response.json(err.message);
+            const user = await updateProfile.execute({
+                userId,
+                name,
+                email,
+                password,
+                oldPassword,
+            });
+            return response.json(user);
+        } catch (err) {
+            return response.json(err.message);
+        }
     }
-  }
 }
